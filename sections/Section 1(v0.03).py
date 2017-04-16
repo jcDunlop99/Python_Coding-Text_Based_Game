45b58b1f7a85a3fcb305388f6a97a37388f6a91b
jcDunlop99 <jcdunlop99@gmail.com>
2017-04-15 20:21:26 -0600

    dont remember

--- sections/Section 1(v0.03).py
+++ sections/Section 1(v0.03).py
@@ -10,23 +10,40 @@ def start():
     a1 = input("Choice: ")
     choice_1(a1)
 
-def s_1():
+def s_1_1():
     print("You wake up in a room in a small puddle of blood, what do you do?")
     print("(1) Check yourself for wounds")
     print("(2) Investigate your surroundings")
     print("(3) Look for a weapon")
-    a2 = input("Choice: ")
-    choice_2(a2)
+    a2_1 = input("Choice: ")
+    choice_2(a2_1)
+
+def s_1_2():
+    print("You wake up in a room in a small puddle of blood, what do you do?")
+    print("(1) Check yourself for wounds")
+    print("(2) Walk through the door")
+    print("(3) Look for a weapon")
+    a2_2 = input("Choice: ")
+    choice_2(a2_2)
+
+def s_1_3():
+    print("You wake up in a room in a small puddle of blood, what do you do?")
+    print("(1) Check yourself for wounds")
+    print("(2) Investigate your surroundings")
+    print("(3) Look for a weapon")
+    a2_3 = input("Choice: ")
+    choice_2(a2_3)
+
 
 def choice_1(a1):
     if a1 == "1":
-        print("You find a small wound behind your left ear")
-        s_1()
+        print("You find a small incession behind your left ear")
+        s_1_1()
     elif a1 == "2":
         print("You find yourself in a concrete cell with a single door")
-        s_1()
+        s_1_2()
     elif a1 == "3":
         print("You find nothing")
-        s_1()
+        s_1_3()
 
 start()
